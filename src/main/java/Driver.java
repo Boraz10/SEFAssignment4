5d2fe1cb158858eea24d0e6efe73076c58e68cf9
Boraz10 <austinborazio@gmail.com>
2026-05-26 10:57:32 +1000

    Basic implementation of required classes. Needs to be updated.

---
+++ src/main/java/Driver.java
@@ -0,0 +1,50 @@
+public class Driver {
+    private String driverID;
+    private String name;
+    private int experienceYears;
+    private String licenseType; // Light, Medium, Heavy, PublicTransport
+    private String address;
+    private String birthdate;
+
+    public Driver(String driverID, String name, int experienceYears, String licenseType, String address, String birthdate) {
+        this.driverID = driverID;
+        this.name = name;
+        this.experienceYears = experienceYears;
+        this.licenseType = licenseType;
+        this.address = address;
+        this.birthdate = birthdate;
+    }
+
+    public String getDriverID() {
+        return driverID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+
+    public void updateDetails(String name, int experienceYears, String licenseType, String address, String birthdate) {
+        this.name = name;
+        this.experienceYears = experienceYears;
+        this.licenseType = licenseType;
+        this.address = address;
+        this.birthdate = birthdate;
+    }
+}
