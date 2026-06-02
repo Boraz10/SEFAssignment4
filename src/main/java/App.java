2b657bcec2d793a6757225e0e5314fb6c7800b7e
Boraz10 <austinborazio@gmail.com>
2026-06-03 08:11:08 +1000

    Added implementation to BusRepository class to interact with the .txt file effectively.
    TODO: Write test cases and test thoroughly

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public class App{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Driver driver = DriverRepository.retrieve("1");
         if(driver == null){
@@ -8,5 +10,14 @@ public class App{
         }
 
         System.out.println(driver.getName());
+
+
+        // Testing bus
+        System.out.println(BusRepository.retrieve("ss").getFuelType());
+
+//        assert bus2 != null;
+//        System.out.println(bus2.getBusID());
+
+
     }
 }
--- src/main/java/BusRepository.java
+++ src/main/java/BusRepository.java
@@ -1,35 +1,91 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class BusRepository {
 
-    // TODO: Instead of arraylist, i think we need to save the data to a txt file
-    private ArrayList<Bus> busList = new ArrayList<>();
+    // Get path to database.
+    static Path path = getDBPath();
 
-    public void add(Bus bus) {
+    // Read the file and store it in an arraylist.
+    private static ArrayList<Bus> busList = readFileToArrayList();
+
+    // Add bus to database
+    public static void add(Bus bus) {
+        // If path is null, bus cannot be added to database.
+        if (path == null) {
+            System.out.println("path is null.");
+            return;
+        }
+
+        // Check if bus of this ID is already present
+        for (Bus other : busList) {
+            String thisBusID = bus.getBusID();
+            if (other.getBusID().equals(thisBusID)) {
+                System.out.println("Bus with ID " + thisBusID + " already exists at index " + busList.indexOf(other));
+                return;
+            }
+        }
+
+        // Usung bufferedWriter to append a string containing bus information to the txt file
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+            writer.write(busToString(bus));
+            writer.newLine();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // Also add the bus to the arraylist
         busList.add(bus);
-
     }
 
-    public void update(String busID, int capacity, double fuelLevel, String fuelType) {
-        for (Bus b : busList) {
+    // Update bus details based on the busID
+    public static void update(String busID, int capacity, double fuelLevel, String fuelType) throws IOException {
+
+        // Iterate through list of busses to try to find the bus in question
+        for (int i = 0; i < busList.size(); ++i) {
+            Bus b = busList.get(i);
             if (b.getBusID().equals(busID)) {
+                // Update bus details (in the arraylist, using the buses update function)
                 b.updateDetails(capacity, fuelLevel, fuelType);
+
+                // Update file
+                updateFile(i, busToString(b), path);
                 return;
             }
         }
         System.out.printf("Could not find bus with id: %s\n", busID);
     }
 
-    public void update(int index, int capacity, double fuelLevel, String fuelType) {
-        if (index >= busList.size()) {
+    // Update bus details based on the index (starting at 0)
+    public static void update(int index, int capacity, double fuelLevel, String fuelType) throws IOException {
+
+        // Check range of index
+        if (index >= busList.size() || index < 0) {
             System.out.printf("Index %d out of range\n", index);
             return;
         }
 
-        busList.get(index).updateDetails(capacity, fuelLevel, fuelType);
+        // Get bus at that index
+        Bus b = busList.get(index);
+
+        // Update that buses details
+        b.updateDetails(capacity, fuelLevel, fuelType);
+
+        // Update the txt file, changing the line (which is index + 1) to then updated bus
+        updateFile(index, busToString(b), path);
     }
 
-    public Bus retrieve(String busID) {
+    // Retrieve bus information based on bus ID (get bus at index)
+    public static Bus retrieve(String busID) {
         for (Bus b : busList) {
             if (b.getBusID().equals(busID)) {
                 return b;
@@ -39,7 +95,96 @@ public class BusRepository {
         return null;
     }
 
-    public int count() {
+    // Return number of buses in database
+    public static int count() {
         return busList.size();
     }
+
+    // Function to read file to the arraylist.
+    public static ArrayList<Bus> readFileToArrayList() {
+        // Temp arraylist
+        ArrayList<Bus> list = new ArrayList<>();
+
+        // Checking if path is null. If it is, it indicates the file doesn't exist
+        if (path == null) {
+            System.out.println("path is null. Creating file...");
+
+            try {
+                // Create a new file if bus.txt doesn't already exist
+                File busFile = new File("./db/bus.txt");
+                if (busFile.createNewFile()) {
+                    System.out.println("Created new file successfully at: " + busFile.getName());
+                    path = getDBPath();
+                }
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        }
+
+        // Read aach line using a buffered reader
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Split each line based on spaces. Each line would be exaclty 4 elements long.
+                // Element[0] = bus ID
+                // Element[1] = fuel Capacity
+                // Element[2] = fuel Level
+                // Element[4] = fuel type
+                String[] elements = line.split("\\s+");
+
+                // Create new bus object based on info from the database
+                Bus bus = new Bus(elements[0], Integer.parseInt(elements[1]), Double.parseDouble(elements[2]), elements[3]);
+
+                // add bus to the temp arraylist
+                list.add(bus);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Return arraylist of buses based on info from database
+        return list;
+    }
+
+    // Helper function which creates string from bus info, useful for writing to the database
+    private static String busToString(Bus bus) {
+        return    bus.getBusID() + " "
+                + bus.getCapacity() + " "
+                + bus.getFuelLevel() + " "
+                + bus.getFuelType();
+
+    }
+
+    // Try to get the path to the database, setting it to null if it can't find it
+    private static Path getDBPath() {
+        String path = "./db/bus.txt";
+        Path dbPath = Path.of(path);
+        if (!Files.exists(dbPath)) {
+            System.out.println("Error: Could not find " + path);
+            return null;
+        }
+        return dbPath;
+    }
+
+    // Update a specified line in the database.
+    private static void updateFile(int index, String content, Path p) throws IOException {
+        // Since line numbers start with 1, we need to add 1 to the index of the line we are updating
+        int lineNumber = index + 1;
+
+        // List of lines
+        List<String> lines = Files.readAllLines(p, StandardCharsets.UTF_8);
+
+        // Check bounds
+        if (lineNumber > 0 && lineNumber <= lines.size()) {
+            lines.set(lineNumber - 1, content);
+        } else {
+            System.out.println("Line number out of bounds");
+            return;
+        }
+
+        // Write changes to file
+        Files.write(path, lines, StandardCharsets.UTF_8);
+    }
 }
+
+
