35a075d24e46bf40932e0687d260b9f944a939ff
ASHLEIGH KIRKPATRICK <s4133235@student.rmit.edu.au--global user.name ASHLEIGH>
2026-06-04 22:17:47 +1000

    Added comments! :)

--- src/test/java/BusIT.java
+++ src/test/java/BusIT.java
@@ -11,53 +11,65 @@ class BusIT {
     @Test
     void busIsWhereSheIsMeantToBe() {
         
-        BusRepository.clearDB();
+        BusRepository.clearDB(); // clear db
+        // create new bus
         Bus bus = new Bus("22222222", "45mn!@opNO", 40, 30.0, "Diesel");
-        BusRepository.add(bus);
+        BusRepository.add(bus); // add bus to repo
+        // store bus object by busID search
         Bus stored = BusRepository.retrieve("22222222");
-        assertNotNull(stored);
-        assertEquals("22222222", stored.getBusID());
-        assertEquals(40, stored.getCapacity());
-        BusRepository.clearDB();
+        assertNotNull(stored); //checks it isnt Null
+        assertEquals("22222222", stored.getBusID()); //Checks Id is same as searched for
+        assertEquals(40, stored.getCapacity()); //ensure capacity is also the same
+        BusRepository.clearDB(); // clear db
     }
 
     // Test 2:
     @Test
     void badBusId() {
 
-        int prev = BusRepository.count();
-        Bus buss = new Bus("8888888", "45mn!@opNO", 40, 30.0, "Diesel"); //driver id repeat
+        int prev = BusRepository.count();       //store previous bus count
+        // Create 2 new busses with invalid bud id (7 , 9). in length
+        Bus buss = new Bus("8888888", "45mn!@opNO", 40, 30.0, "Diesel"); 
         Bus busses = new Bus("888888888", "45mn!@opNO", 40, 30.0, "Diesel");
+        // Add busses to repo
         BusRepository.add(buss);
         BusRepository.add(busses);
+        //Attempt to reteive bus from ID
         Bus stored = BusRepository.retrieve("8888888");
         Bus cryoChamber = BusRepository.retrieve("8888888");
+        // if null test passed
         assertNull(stored);
         assertNull(cryoChamber);
+        // count should not have gone up
         assertEquals(prev, BusRepository.count());
-        BusRepository.clearDB();
+        BusRepository.clearDB(); // clear db
     }
 
     // Test 3:
     @Test
     void updateBusCheck() throws IOException {
-    
+        // create new bus 
         Bus bus = new Bus("99999999", "45mn!@opNO", 40, 30.0, "Diesel");
+        // add bus
         BusRepository.add(bus);
+        // update capacity to 20
         BusRepository.update("99999999", "45mn!@opNO", 20, 30.0, "Diesel");
+        // check that capcity ==2
         assertEquals(20, BusRepository.retrieve("99999999").getCapacity());
-        BusRepository.clearDB();
+        BusRepository.clearDB(); // clear db
 
     }
 
     // Test 4:
     @Test
     void busAccountedFor() {
-        int prev = BusRepository.count();
+        int prev = BusRepository.count(); // previous count value
+        // create new bus
         Bus bus = new Bus("66666666", "45mn!@opNO", 40, 30.0, "Diesel");
-        BusRepository.add(bus);
+        BusRepository.add(bus); // add bus
+        // checks count has been updates.
         assertEquals(prev + 1,  BusRepository.count());
-        BusRepository.clearDB();
+        BusRepository.clearDB(); // clear db
 
     }
 }
